73357abda861e7d220c4dbfd23884c5ae5c32ca6
mbp4 <mblanpon@myuax.com>
2024-09-18 08:54:47 +0200

    Mensaje de bienvenida dependiente de la hora

--- app/src/main/java/com/example/taller1/MainActivity.kt
+++ app/src/main/java/com/example/taller1/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.taller1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.taller1.ui.theme.Taller1Theme
+import java.util.Calendar
+import java.util.Calendar.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +29,10 @@ class MainActivity : ComponentActivity() {
             Taller1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .wrapContentSize(Alignment.Center)
                     )
                 }
             }
@@ -30,18 +40,27 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier) {
+    val hora = Calendar.getInstance().get(HOUR_OF_DAY)
+    val bienvenida = when{
+        hora < 12 -> "Buenos dias"
+        hora < 20 -> "Buenas tardes"
+        else -> "Buenas noches"
+    }
+
+    Text(text = bienvenida.toString())
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     Taller1Theme {
-        Greeting("Android")
+        Greeting(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+        )
     }
 }
