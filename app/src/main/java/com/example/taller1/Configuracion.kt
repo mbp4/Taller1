d00515b8913dd41dfa40300466d9df98ec98f684
mbp4 <mblanpon@myuax.com>
2024-09-18 18:59:56 +0200

    configuracion actualizada

--- app/src/main/java/com/example/taller1/Configuracion.kt
+++ app/src/main/java/com/example/taller1/Configuracion.kt
@@ -1,5 +1,42 @@
 package com.example.taller1
 
-class Configuracion {
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
+class Configuracion: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
 }
+
+@Composable
+fun BotonMain(){
+
+    Spacer(modifier = androidx.compose.ui.Modifier.height(16.dp))
+
+    val context = LocalContext.current
+    Button(onClick = {
+        context.startActivity(
+            Intent(
+                context,
+                MainActivity::class.java
+            )
+        )
+    }) {
+        Text("INICIO")
+    }
+}
+
+
