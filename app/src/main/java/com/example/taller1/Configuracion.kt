51eeee4cd9cd10a8d68aa5ce47a388a159bc26ed
mbp4 <mblanpon@myuax.com>
2024-09-19 12:59:34 +0200

    configuracion con preview

--- app/src/main/java/com/example/taller1/Configuracion.kt
+++ app/src/main/java/com/example/taller1/Configuracion.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class Configuracion: ComponentActivity() {
@@ -37,6 +38,12 @@ fun BotonMain(){
     }) {
         Text("INICIO")
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview2() {
+    BotonMain()
 }
 
 
