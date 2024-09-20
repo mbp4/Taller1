60c0dbfca744f31bfc7a542870648de7a18ba6dd
mbp4 <mblanpon@myuax.com>
2024-09-20 23:55:09 +0200

    modificaciones para añadir un layout al proyecto

--- app/src/main/java/com/example/taller1/Config.kt
+++ app/src/main/java/com/example/taller1/Config.kt
@@ -1,9 +1,13 @@
 package com.example.taller1
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
@@ -14,6 +18,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 
 class Config: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,18 +40,26 @@ fun GreetingPreview2() {
 @Composable
 fun Configur() {
 
-    Spacer(modifier = androidx.compose.ui.Modifier.height(16.dp))
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,)
+    {
 
-    val context = LocalContext.current
-    Button(onClick = {
-        context.startActivity(
-            Intent(
-                context,
-                Inicio::class.java
+
+        Spacer(modifier = androidx.compose.ui.Modifier.height(16.dp))
+
+        val context2 = LocalContext.current
+        Button(onClick = {
+            context2.startActivity(
+                Intent(
+                    context2,
+                    Inicio::class.java
+                )
             )
-        )
-    }) {
-        Text("INICIO")
+        }) {
+            Text("INICIO")
+        }
     }
 
 }
+
--- app/src/main/java/com/example/taller1/MainActivity.kt
+++ app/src/main/java/com/example/taller1/MainActivity.kt
@@ -2,86 +2,35 @@ package com.example.taller1
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.taller1.ui.theme.Taller1Theme
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.Calendar
-import java.util.Calendar.*
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Taller1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize()
-                            .wrapContentSize(Alignment.Center)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
 
-@Composable
-fun Greeting(modifier: Modifier) {
-    Column {
-        Spacer(modifier = Modifier.height(20.dp))
+        val greetingTextView = findViewById<TextView>(R.id.greetingText)
+        val inicioButton = findViewById<Button>(R.id.inicioButton)
 
-        val hora = Calendar.getInstance().get(HOUR_OF_DAY)
+
+        val hora = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
         val bienvenida = when {
-            hora < 13 -> "Buenos dias"
+            hora < 13 -> "Buenos días"
             hora < 20 -> "Buenas tardes"
             else -> "Buenas noches"
         }
 
-        Text(text = bienvenida.toString())
 
-        Spacer(modifier = Modifier.height(16.dp))
+        greetingTextView.text = bienvenida
 
-        val context = LocalContext.current
-        Button(onClick = {
-            context.startActivity(
-                Intent(
-                    context,
-                    Inicio::class.java
-                )
-            )
-        }) {
-            Text("INICIO")
+
+        inicioButton.setOnClickListener {
+            val intent = Intent(this, Inicio::class.java)
+            startActivity(intent)
         }
-    }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun Preview() {
-    Taller1Theme {
-        Greeting(modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
-        )
     }
 }
