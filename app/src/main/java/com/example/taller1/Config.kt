6b964b8193fc1ff3d0985296acbc707fa2d4ee56
mbp4 <mblanpon@myuax.com>
2024-09-20 20:15:33 +0200

    Boton en la configuracion para volver al inicio

--- app/src/main/java/com/example/taller1/Config.kt
+++ app/src/main/java/com/example/taller1/Config.kt
@@ -1,11 +1,19 @@
 package com.example.taller1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import java.lang.reflect.Modifier
+import androidx.compose.foundation.layout.height
 
 class Config: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +27,24 @@ class Config: ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
-   Text("Hola Mundo")
+   Configur();
+}
+
+@Composable
+fun Configur() {
+
+    Spacer(modifier = androidx.compose.ui.Modifier.height(16.dp))
+
+    val context = LocalContext.current
+    Button(onClick = {
+        context.startActivity(
+            Intent(
+                context,
+                Inicio::class.java
+            )
+        )
+    }) {
+        Text("INICIO")
+    }
+
 }
